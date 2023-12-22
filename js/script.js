6d0007555b42cbb3fe9f968e1fd91d0cee62b3cd
Juan Ignacio Caprioli <109362877+CHANOCHOCA@users.noreply.github.com>
2023-12-22 18:32:16 -0300

    Boostrap + responsive celular + responsive tablet

--- js/script.js
+++ js/script.js
@@ -1,65 +1,40 @@
-let lastScroll = 0;
+//navbar
+let prevScrollpos = window.scrollY;
 
-window.addEventListener("scroll", () => {
-    const currentScroll = window.scrollY;
-    const header = document.querySelector("header");
-    const navMenu = document.querySelector(".menu-desplegable");
+window.onscroll = function () {
+    const currentScrollPos = window.scrollY;
+    const navbarButton = document.querySelector(".navbar-toggler");
 
-    if (currentScroll > lastScroll) {
-        header.style.top = "-100%";
-
-        // Verificar si la clase "visible1" está activada en el menú desplegable
-        if (navMenu.classList.contains("visible1")) {
-            // Si la clase está activada, establecer top en "0"
-            header.style.top = "0";
+    if (currentScrollPos > prevScrollpos) {
+        document.querySelector(".navbar").style.top = "-101px";
+        if (navbarButton.getAttribute("aria-expanded") === "true") {
+            document.querySelector(".navbar").style.top = "0";
         }
     } else {
-        header.style.top = "0";
+        document.querySelector(".navbar").style.top = "0";
     }
-    lastScroll = currentScroll;
-});
 
-document.addEventListener("DOMContentLoaded", function() {
-    const navToggle = document.querySelector(".iconos-responsive");
-    const navMenu = document.querySelector(".menu-desplegable");
+    prevScrollpos = currentScrollPos;
+};
 
-    navToggle.addEventListener("click", () => {
-        navMenu.classList.toggle("visible1");
-    });
-});
+//testimonios
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".boton-acordeon");
 
-window.onload = function() {
-    var titulos = document.querySelectorAll('.titulo');
-    var contenidos = document.querySelectorAll('.contenido');
-
-    titulos.forEach(function(titulo, index) {
-        titulo.addEventListener('click', function(event) {
-            // Prevent the default click behavior
-            event.preventDefault();
-
-            contenidos.forEach(function(contenido) {
-                // Change class to 'contenido oculto'
-                contenido.className = 'contenido oculto';
-            });
-            contenidos[index].className = 'contenido visible flexbox1 espacio-flex';
+    buttons.forEach(function (button, index) {
+        button.addEventListener("click", function () {
+            toggleImage(index);
         });
     });
 
-    // Show the first .contenido by default
-    if (contenidos.length > 0) {
-        contenidos[0].className = 'contenido visible flexbox1 espacio-flex';
+    function toggleImage(index) {
+        buttons.forEach(function (btn, i) {
+            const img = btn.querySelector("img");
+            img.src = i === index ? toggleSrc(img.src) : "assets/index/boton.svg";
+        });
     }
-};
 
-function changeTestimonial(index) {
-    const testimonials = document.querySelectorAll('.testimonial');
-    const icons = document.querySelectorAll('.flexbox2 .buscar');
-
-    // Remove active class from all testimonials and icons
-    testimonials.forEach(testimonial => testimonial.classList.remove('active'));
-    icons.forEach(icon => icon.src = 'assets/index/boton.svg');
-
-    // Add active class to the selected testimonial and change the icon
-    testimonials[index - 1].classList.add('active');
-    icons[index - 1].src = 'assets/index/boton-relleno.svg';
-}
+    function toggleSrc(src) {
+        return src.endsWith("boton-relleno.svg") ? "assets/index/boton.svg" : "assets/index/boton-relleno.svg";
+    }
+});
